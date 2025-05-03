485e2179470cba202a1b3493c60c92b84068153f
AlexChaverra <alchaverrav@unal.edu.co>
2025-05-03 12:56:27 -0500

    Intento2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -17,6 +17,9 @@ def main():
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
 
+    # input_folder = "data/input"
+    # output_folder = "data/output"
+
     all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
     words = split_into_words(all_lines)
